cbc4c8d17715e9f6f4f53f68ae78557de2f8c1d8
LMX29 <soitajoel01@gmail.com>
2026-09-09 11:19:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function makeRed () {
   document. getElementById("home") .style. color="red";
 }
 
+function makeBlue () {
+  document. getElementById9("home") .style. color="blue";
+{
+
+ function darkMode () {
+   document.body.style.backgroundColor="black";
+
+   document.body.style.color="white";
+ }
+
+ function lightMode () {
+   document.body.style.backgroundColor="white";
+ } 
